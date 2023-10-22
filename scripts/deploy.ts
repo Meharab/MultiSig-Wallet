9497dd20b523c89e6ccc6f25712d4f6c19a28d91
meharab <meharablatif@hotmail.com>
2023-10-22 19:49:17 +0600

    contract complete

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,18 @@
 import { ethers } from "hardhat";
 
+const ADDRESS1 = "0x1234567";
+const ADDRESS2 = "0xabcdefgh";
+const PASSNUM = 2;
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const factory = await ethers.getContractFactory("MultiSigWallet");
+  const contract = await factory.deploy( ADDRESS1, ADDRESS2, PASSNUM );
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  await contract.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Lottery deployed to:", contract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
